b63c5717422792f54719dbbbd1a45c5dd27c41ca
Jan Huber <jan.huber@bruker.com>
2023-09-27 10:18:39 +0200

    worktime: initial commit

---
+++ worktime.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from datetime import datetime, timedelta
+
+
+def print_timedelta_hours_minutes(time_delta):
+    td_hours = time_delta.seconds // 3600
+    td_minutes = (time_delta.seconds // 60) % 60
+    return f"{td_hours}:{td_minutes:02d}"
+
+
+if __name__ == "__main__":
+    # get start time from user input
+    t_input = input("Enter Check-In Time [hh:mm]: ")
+    t_start = datetime.strptime(t_input, "%H:%M")
+
+    # calculate theoretical end time w/ and w/o breakfast break
+    t_end_regular = t_start + timedelta(hours=8.5)
+    t_end_breakfast = t_end_regular + timedelta(minutes=15)
+    print("regular clock out: ", t_end_regular.strftime("%H:%M"))
+    print("include breakfast: ", t_end_breakfast.strftime("%H:%M"))
+
+    # get current time
+    t_now = datetime.strptime(datetime.now().strftime("%H:%M"), "%H:%M")
+    # calculate current working time
+    t_working = t_now - t_start
+    # after working 6 hours, 30min are deducted automatically
+    if t_working > timedelta(hours=6):
+        t_working = t_working - timedelta(minutes=30)
+        t_working_breakfast = t_working - timedelta(minutes=15)
+    # after working 9 hours, 15min are additionally deducted automatically
+    if t_working > timedelta(hours=9):
+        print("")
+        print("!!! 9 hours passed, 15min break additionally deducted !!!")
+        t_working = t_working - timedelta(minutes=15)
+        t_working_breakfast = t_working
+
+    print("")
+
+    print(f"hours worked: {print_timedelta_hours_minutes(t_working)}")
+    print(f"incl. coffee: {print_timedelta_hours_minutes(t_working_breakfast)}")
+
+    print("")
+
+    # calculate overtime
+    if t_working >= timedelta(hours=8):
+        t_over = t_working - timedelta(hours=8)
+        t_over_breakfast = t_working_breakfast - timedelta(hours=8)
+        print(f"overtime: {print_timedelta_hours_minutes(t_over)}")
+        print(f"w/coffee: {print_timedelta_hours_minutes(t_over_breakfast)}")
+    else:
+        t_remaining = timedelta(hours=8) - t_working
+        print(f"remaining: {print_timedelta_hours_minutes(t_remaining)}")
