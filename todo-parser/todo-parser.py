aa6505a625f22ff0f4f146307d6299bdf0b3d654
Jan Huber <jan.huber@bruker.com>
2024-02-13 14:49:23 +0100

    todo-parser: add functions to filter for open/done todos

--- todo-parser/todo-parser.py
+++ todo-parser/todo-parser.py
@@ -1,6 +1,26 @@
 TODO_LIST_PATH = "/home/jahu/_vm-share/notes/obsidian/work/_todo/parser-test.md"
 
 
+def filter_open_todos(todo_list: list):
+    open_todos = []
+
+    for todo in todo_list:
+        if todo.find("[ ]") != -1:
+            open_todos.append(todo)
+
+    return open_todos
+
+
+def filter_done_todos(todo_list: list):
+    done_todos = []
+
+    for todo in todo_list:
+        if todo.find("[ ]") == -1:
+            done_todos.append(todo)
+
+    return done_todos
+
+
 def get_todo_list():
     with open(TODO_LIST_PATH, "r", encoding="utf-8") as todo_file:
         todo_l = todo_file.readlines()
@@ -10,5 +30,11 @@ def get_todo_list():
 
 if __name__ == "__main__":
     todo_list = get_todo_list()
-    for todo in todo_list:
+    open_list = filter_open_todos(todo_list)
+    done_list = filter_done_todos(todo_list)
+
+    for todo in open_list:
+        print(f"{todo.strip()}")
+
+    for todo in done_list:
         print(f"{todo.strip()}")
