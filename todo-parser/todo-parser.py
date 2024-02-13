3b4b547027830d7b848d4439213b10d4bd644971
Jan Huber <jan.huber@bruker.com>
2024-02-13 16:54:11 +0100

    todo-parser: add functions to filter for overdue or due today tasks

--- todo-parser/todo-parser.py
+++ todo-parser/todo-parser.py
@@ -1,4 +1,36 @@
-TODO_LIST_PATH = "/home/jahu/_vm-share/notes/obsidian/work/_todo/parser-test.md"
+from datetime import date
+
+TODO_LIST_PATH = "/home/jahu/_vm-share/src/python/todo-parser/parser-test.md"
+
+
+def filter_overdue(todo_list: list):
+    overdue = []
+
+    today = date.today()
+
+    for todo in todo_list:
+        todo_split = todo.split("|")
+        due = todo_split[1].split(".")
+        due_date = date(int(due[2]), int(due[1]), int(due[0]))
+        if due_date < today:
+            overdue.append(todo)
+
+    return overdue
+
+
+def filter_due_today(todo_list: list):
+    due_today = []
+
+    today = date.today()
+
+    for todo in todo_list:
+        todo_split = todo.split("|")
+        due = todo_split[1].split(".")
+        due_date = date(int(due[2]), int(due[1]), int(due[0]))
+        if due_date == today:
+            due_today.append(todo)
+
+    return due_today
 
 
 def filter_open_todos(todo_list: list):
@@ -30,11 +62,15 @@ def get_todo_list():
 
 if __name__ == "__main__":
     todo_list = get_todo_list()
-    open_list = filter_open_todos(todo_list)
-    done_list = filter_done_todos(todo_list)
 
-    for todo in open_list:
+    for todo in filter_overdue(todo_list):
         print(f"{todo.strip()}")
 
-    for todo in done_list:
-        print(f"{todo.strip()}")
+    # for todo in filter_due_today(todo_list):
+    #     print(f"{todo.strip()}")
+
+    # for todo in filter_open_todos(todo_list):
+    #     print(f"{todo.strip()}")
+
+    # for todo in filter_done_todos(todo_list):
+    #     print(f"{todo.strip()}")
